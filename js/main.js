0df0727c5db53e3bcd3b118133f5f4d73fe39d5e
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-09-04 10:44:22 +0200

    Versione ultima corretta

--- js/main.js
+++ js/main.js
@@ -1,91 +1,100 @@
 $(document).ready(
   function(){
 
-    /*blocco tariffazione*/
-    $("#denaro").mouseenter(
+    $(".header-right > .list-right > li").hover(
       function(){
-        $(".denaro-menu").show();
-        $(".investimenti-menu").hide();
-        $(".controllo-menu").hide();
-        $(".altro-menu").hide();
-        $(".abbonamenti-menu").hide();
-      }
-    );
-
-    $(".denaro-menu").mouseleave(
-      function(){
-        $(".denaro-menu").hide();
+        $(this).children(".dropdown-menu").toggle();
       }
     );
 
 
-    /*blocco investimenti*/
-    $("#investimenti").hover(
-      function(){
-        $(".investimenti-menu").show();
-        $(".denaro-menu").hide();
-        $(".controllo-menu").hide();
-        $(".altro-menu").hide();
-        $(".abbonamenti-menu").hide();
-      }
-    );
 
-    $(".investimenti-menu").mouseleave(
-      function(){
-        $(".investimenti-menu").hide();
-      }
-    );
 
-    /*blocco controllo*/
-    $("#controllo").hover(
-      function(){
-        $(".controllo-menu").show();
-        $(".investimenti-menu").hide();
-        $(".denaro-menu").hide();
-        $(".altro-menu").hide();
-        $(".abbonamenti-menu").hide();
-      }
-    );
-
-    $(".controllo-menu").mouseleave(
-      function(){
-        $(".controllo-menu").hide();
-      }
-    );
-
-    /*blocco altro*/
-    $("#altro").hover(
-      function(){
-        $(".altro-menu").show();
-        $(".controllo-menu").hide();
-        $(".investimenti-menu").hide();
-        $(".denaro-menu").hide();
-        $(".abbonamenti-menu").hide();
-      }
-    );
-
-    $(".altro-menu").mouseleave(
-      function(){
-        $(".altro-menu").hide();
-      }
-    );
-
-    /*blocco abbonamenti*/
-    $("#abbonamenti").hover(
-      function(){
-        $(".abbonamenti-menu").show();
-        $(".altro-menu").hide();
-        $(".controllo-menu").hide();
-        $(".investimenti-menu").hide();
-        $(".denaro-menu").hide();
-      }
-    );
-
-    $(".abbonamenti-menu").mouseleave(
-      function(){
-        $(".abbonamenti-menu").hide();
-      }
-    );
+    // /*blocco tariffazione*/
+    // $("#denaro").mouseenter(
+    //   function(){
+    //     $(".denaro-menu").show();
+    //     $(".investimenti-menu").hide();
+    //     $(".controllo-menu").hide();
+    //     $(".altro-menu").hide();
+    //     $(".abbonamenti-menu").hide();
+    //   }
+    // );
+    //
+    // $(".denaro-menu").mouseleave(
+    //   function(){
+    //     $(".denaro-menu").hide();
+    //   }
+    // );
+    //
+    //
+    // /*blocco investimenti*/
+    // $("#investimenti").hover(
+    //   function(){
+    //     $(".investimenti-menu").show();
+    //     $(".denaro-menu").hide();
+    //     $(".controllo-menu").hide();
+    //     $(".altro-menu").hide();
+    //     $(".abbonamenti-menu").hide();
+    //   }
+    // );
+    //
+    // $(".investimenti-menu").mouseleave(
+    //   function(){
+    //     $(".investimenti-menu").hide();
+    //   }
+    // );
+    //
+    // /*blocco controllo*/
+    // $("#controllo").hover(
+    //   function(){
+    //     $(".controllo-menu").show();
+    //     $(".investimenti-menu").hide();
+    //     $(".denaro-menu").hide();
+    //     $(".altro-menu").hide();
+    //     $(".abbonamenti-menu").hide();
+    //   }
+    // );
+    //
+    // $(".controllo-menu").mouseleave(
+    //   function(){
+    //     $(".controllo-menu").hide();
+    //   }
+    // );
+    //
+    // /*blocco altro*/
+    // $("#altro").hover(
+    //   function(){
+    //     $(".altro-menu").show();
+    //     $(".controllo-menu").hide();
+    //     $(".investimenti-menu").hide();
+    //     $(".denaro-menu").hide();
+    //     $(".abbonamenti-menu").hide();
+    //   }
+    // );
+    //
+    // $(".altro-menu").mouseleave(
+    //   function(){
+    //     $(".altro-menu").hide();
+    //   }
+    // );
+    //
+    // /*blocco abbonamenti*/
+    // $("#abbonamenti").hover(
+    //   function(){
+    //     $(".abbonamenti-menu").show();
+    //     $(".altro-menu").hide();
+    //     $(".controllo-menu").hide();
+    //     $(".investimenti-menu").hide();
+    //     $(".denaro-menu").hide();
+    //   }
+    // );
+    //
+    // $(".abbonamenti-menu").mouseleave(
+    //   function(){
+    //     $(".abbonamenti-menu").hide();
+    //   }
+    // );
 
 
 
